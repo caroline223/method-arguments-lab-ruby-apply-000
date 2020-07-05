2126ae9cd3c3e8af6b9bfe811c72efb049b54aba
Caroline Forrester <carolineforrester22@gmail.com>
2020-07-05 07:45:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-def introduction_with_language(name,language)
-  puts "Hi, my name is #{name}, and I am learning to program in #{language}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
-introduction_with_language("Dan","Ember.js")
+introduction("Dan")
